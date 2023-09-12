3e927e9d761982b269a0613f894b121dd105a2cc
Blazskills <ilesanmiisaac@gmail.com>
2023-09-12 12:27:02 +0100

    added dark mode

--- src/pages/nigeriamapage/NigeriaMapPage.jsx
+++ src/pages/nigeriamapage/NigeriaMapPage.jsx
@@ -5,10 +5,10 @@ export const NigeriaMapPage = () => {
     <section className=" w-full h-screen bg-green-50 ">
       <div className=" xl:min-h-[80vh] bg-primary bg-hero bg-cover bg-center text-white my-auto flex flex-col justify-center items-center laptop1024up:min-h-[100vh]">
         <div className="mobile425down:w-full w-3/5 tablet768up:w-4/5 laptop1024up:w-[75%]  h-[50vh] laptop1024up:h-[64vh] flex flex-col justify-center items-start gap-y-5  backdrop-filter bg-opacity-40 p-5 rounded-sm tablet768up:backdrop-blur-md">
-          <h1 className="text-[24px]  tablet768up:text-[40px] laptop1024up:text-[80px] laptop1024up:leading-[90px] tablet768up:leading-[40px] uppercase font-palanquin font-bold screen2550up:text-[150px] screen2550up:leading-[150px]">
+          <h1 className="dark:text-white text-[24px]  tablet768up:text-[40px] laptop1024up:text-[80px] laptop1024up:leading-[90px] tablet768up:leading-[40px] uppercase font-palanquin font-bold screen2550up:text-[150px] screen2550up:leading-[150px]">
             Awaken to a different world in Nigeria Travel Level
           </h1>
-          <p className="text-white font-palanquin font-semibold w-4/5 leading-19 mb-5 screen2550up:text-[30px] screen2550up:leading-[40px]">
+          <p className="text-white dark:text-white font-palanquin font-semibold w-4/5 leading-19 mb-5 screen2550up:text-[30px] screen2550up:leading-[40px]">
             Move your imagination, discover the unexpected and experience
             happiness on earth with a this simple web app helps you visualize
             how well-traveled you are in the Nigeria.
